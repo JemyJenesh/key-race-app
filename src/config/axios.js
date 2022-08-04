fca98f70b628c87fc3cf95380fbab69df50bc8ea
jenesh <jenesh@dzangolab.com>
2022-08-04 21:50:58 +0545

    Feat(env) - add env

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,5 +1,6 @@
 import Axios from "axios";
 
 export const axios = Axios.create({
-  baseURL: process.env.API_URL + "/api" || "http://localhost:5000/api",
+  baseURL:
+    process.env.REACT_APP_API_URL + "/api" || "http://localhost:5000/api",
 });
