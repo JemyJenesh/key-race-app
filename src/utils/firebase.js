f40479dd50075a114f75c87995fd6fafa49b94af
jenesh <jenesh@dzangolab.com>
2022-08-13 10:35:58 +0545

    use env for firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCw5QjZLfLOpR11X4bC_cWnqdSvr4vfnsM",
-  authDomain: "key-race.firebaseapp.com",
-  projectId: "key-race",
-  storageBucket: "key-race.appspot.com",
-  messagingSenderId: "690674375362",
-  appId: "1:690674375362:web:605448c746ab667c22395b",
-  measurementId: "G-F0Q9GCGWC3",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
